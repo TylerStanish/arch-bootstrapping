ba2ab6888a7ae0647af8526b7dc0ad449d2faf32
Tyler Stanish <tystanish@gmail.com>
2020-06-04 14:15:17 -0500

    added after-chroot

---
+++ after-chroot.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+function print_error_message() {
+  echo 'failed...'
+}
+
+trap 'print_error_message' ERR
+read -p "Enter timezone (Central, Eastern, etc):" timezone
+read -p "Enter hostname:" hostname
+
+
+ln -sf /usr/share/zoneinfo/US/$timezone
+hwclock --systohco
+
+sed -i 's/# en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.conf
+echo "$hostname" > /etc/hostname
+echo "
+127.0.0.1 localhost
+::1   localhost
+127.0.1.1 $hostname.localdomain  $hostname
+" > /etc/hosts
+
+passwd
+grub-install --target=x86_64-efi --efi-directory=/efi
+grub-mkconfig -o /efi/grub
